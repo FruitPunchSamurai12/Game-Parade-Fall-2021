ad602f28dd71863947b5fa691faa1b672890551b
pougrivioure <klanko212@hotmail.fr>
2021-11-18 17:04:19 +0100

    MusicSystemFix
    Should work this f*** time

--- Game Parade Fall 2021/Assets/Scripts/Music.cs
+++ Game Parade Fall 2021/Assets/Scripts/Music.cs
@@ -7,14 +7,14 @@ public class Music : MonoBehaviour
     private FMOD.Studio.EventInstance music;
     // Start is called before the first frame update
     void Start()
-    {      
-        music = FMODUnity.RuntimeManager.CreateInstance("events:/Music");
+    {
+        music = FMODUnity.RuntimeManager.CreateInstance("event:/Music");
         music.start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
